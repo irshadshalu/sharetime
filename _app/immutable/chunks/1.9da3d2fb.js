8837341999faee2c08e535149397431460d82d3c
Irshad P I <irshadpi77@gmail.com>
2023-04-15 05:28:18 +0530

    Updates

--- _app/immutable/chunks/1.9da3d2fb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.a8f9c037.js";export{t as component};
---
+++ _app/immutable/chunks/1.c55b6d23.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.e66c1e3e.js";export{t as component};
