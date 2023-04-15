fa684d07b3012921ba1042dddee937ea6875aaaa
Irshad P I <irshadpi77@gmail.com>
2023-04-15 15:07:51 +0530

    Updates

--- _app/immutable/entry/404-page.svelte.6941390b.js
+++
@@ -1,1 +0,0 @@
-import{S as o,i,s as r,q as c,r as l,b as u,H as e,h as f}from"../chunks/index.9089437a.js";function p(s){let t;return{c(){t=c("This page was not found")},l(a){t=l(a,"This page was not found")},m(a,n){u(a,t,n)},p:e,i:e,o:e,d(a){a&&f(t)}}}class h extends o{constructor(t){super(),i(this,t,null,p,r,{})}}export{h as default};
